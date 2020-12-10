c4b07994045e62a54b6b5deaee55956f54bd92b0
Yoichi Ishibashi <yoichi1484@gmail.com>
2020-12-10 19:56:43 +0900

    Update fix_glove_file.py

--- src/fix_glove_file.py
+++ src/fix_glove_file.py
@@ -1,15 +1,4 @@
-'''
-    Step1. Download pre-trained GloVe(glove.42B.300d.txt) 
-    from( https://nlp.stanford.edu/projects/glove/) 
-    and put it in data/
-
-    Step2. Run this script.
-'''
-
 from gensim.scripts.glove2word2vec import glove2word2vec
-
-if __name__ == '__main__':
-    path_old = '../data/glove.42B.300d.txt' 
-    path_new = '../data/glove.42B.300d_gensim.txt' 
-
-    _ = glove2word2vec(path_old, path_new)
+path_old = '../data/glove.42B.300d.txt' 
+path_new = '../data/glove.42B.300d_gensim.txt' 
+glove2word2vec(path_old, path_new)
